556a9cbdb08369ee760089d81c3110c5fff8b5ca
Sagar <ganesha4us1@gmail.com>
2025-10-02 01:15:58 -0700

    update

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.genai as genai
-
-client = genai.Client()
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=["Hello Gemini!"]
-)
-print(response.text)
